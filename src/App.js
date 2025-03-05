2f9a6b23984d4cf3efb3d453678f7ac6a7600029
Superkid <kevinchristensen76923@gmail.com>
2025-03-05 21:02:20 +0700

    a

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
 import "./App.css";
 import { useState } from "react";
-const quotes = [
-	"Success is not final, failure is not fatal: it is the courage to continue that counts.",
-	"Do what you can, with what you have, where you are.",
-	"The only limit to our realization of tomorrow is our doubts of today.",
-	"Believe you can and you're halfway there.",
-	"Hardships often prepare ordinary people for an extraordinary destiny.",
-	"Opportunities don’t happen, you create them.",
-	"Your time is limited, so don’t waste it living someone else’s life.",
-	"Great things never come from comfort zones.",
-	"It does not matter how slowly you go as long as you do not stop.",
-	"A journey of a thousand miles begins with a single step.",
-];
+import quotesData from "./quotes.json";
+const quotes = quotesData.quotes;
 
 function App() {
 	const [quote, setQuote] = useState("");
